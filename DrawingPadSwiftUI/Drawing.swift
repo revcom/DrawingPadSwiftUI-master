640640c510e0bc14718bdc08aeaff023af73bca6
robert Crago <robert@revComputing.com>
2021-07-02 11:57:44 +1000

    Initial Commit

---
+++ DrawingPadSwiftUI/Drawing.swift
@@ -0,0 +1,19 @@
+//
+//  Drawing.swift
+//  DrawingPadSwiftUI
+//
+//  Created by Martin Mitrevski on 19.07.19.
+//  Copyright © 2019 Mitrevski. All rights reserved.
+//
+
+import CoreGraphics
+import SwiftUI
+
+struct Drawing {
+    var points: [CGPoint] = [CGPoint]()
+}
+
+struct Shape {
+    var colour: Color
+    var width: CGFloat
+}
---
+++ DrawingPadSwiftUI/DrawingViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  DrawingViewModel.swift
+//  DrawingPadSwiftUI
+//
+//  Created by Robert Crago on 2/7/21.
+//  Copyright © 2021 Mitrevski. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class DrawingViewModel : ObservableObject {
+
+    func loadDrawing() -> Drawing? {
+        return nil
+    }
+    
+    func saveDrawing(drawing: Drawing) {
+        
+    }
+}
